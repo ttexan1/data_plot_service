d3b39735a9a78558d2717e68b8bbd7170d94df8d
ttexan1 <ttexan.developper@gmail.com>
2019-06-02 13:01:21 +0900

    temp

--- static/plotbtc.js
+++ static/plotbtc.js
@@ -8,15 +8,33 @@ function plotbtc() {
     param["end"] = document.getElementById("end").value;
     var query = jQuery.param(param);
 
-// Query with a new parameter 
+// Query with a new parameter
     $.get("/plot/btc" + "?" + query, function(data) {
+        document.getElementById("plotimg").src = data;
+    });
+};
+
+function plotLivingCost() {
+// Buid query parameter
+    // var param = {};
+    // param["start"] = document.getElementById("start").value;
+    // param["end"] = document.getElementById("end").value;
+    // var query = jQuery.param(param);
+    var query = "l=10"
+// Query with a new parameter
+    $.get("/living_cost" + "?" + query, function(data) {
         document.getElementById("plotimg").src = data;
     });
 };
 //
 // Register Event handler
 //
-document.getElementById("plot").addEventListener("click", function(){
-    plotbtc();
+
+
+document.getElementById("myplot").addEventListener("click", function(){
+    plotLivingCost();
 }, false);
-plotbtc();
+// document.getElementById("plot").addEventListener("click", function(){
+//     plotbtc();
+// }, false);
+// plotbtc();
